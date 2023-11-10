47607b09042ba5955dc6c1e0582045e71af6e785
yoon <whyjmlearn@gmail.com>
2023-11-10 12:56:14 +0900

    all

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
 import logo from './logo.svg';
-import './App.css';
 
 function App() {
+  const jm = "하이~clone test"
+  function test() {
+    return 100;
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div> {jm}{test()} </div>
+      
     </div>
   );
 }
